5b70c877aee4151db483179af1dccbc8ff727b7c
M.Hamza Shahzad <hamzashahzad664343@gmail.com>
2024-03-30 18:35:25 +0500

    a

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def predict():
 
 if __name__ == '__main__':
     print_dataset_info()  # Print dataset info when the script is run directly
-    app.run(debug=True)
+    app.run(debug=True)   # push
